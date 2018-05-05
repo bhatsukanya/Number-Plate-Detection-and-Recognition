fb9f70698ed2fedd86ac5b476003cd0e92d23109
bhatsukanya <36473876+bhatsukanya@users.noreply.github.com>
2018-05-05 08:12:46 +0530

    Update LocalizationDemo.m

--- LocalizationDemo.m
+++ LocalizationDemo.m
@@ -2,5 +2,6 @@
 testImg=imread(strcat(path,'/',testImgName));
 plateImg=localize(testImg);
 targetFolder='./Localization Results/';
+mkdir(targetFolder);
 targetFilePath=strcat(targetFolder,testImgName);
 imwrite(plateImg,targetFilePath);
